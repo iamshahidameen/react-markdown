3125c9592273a19a1096c6a44d155ae1bc0bb54e
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-09 01:58:23 +0500

    set state variable for textarea

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [markdown, setMarkDown] = useState('### Markdown Preview');
   return (
     <main>
       <section className="markdown">
-        <textarea className="input"></textarea>
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkDown(e.target.value)}
+        ></textarea>
         <article className="result">
           <h1>markdown preview</h1>
           <h3>no</h3>
